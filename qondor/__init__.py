0740c22057ca954a8aad338303383592b4cef5de
Thomas Klijnsma <klijnsma@fnal.gov>
2020-02-05 15:53:42 -0600

    minor fix

--- qondor/__init__.py
+++ qondor/__init__.py
@@ -12,3 +12,12 @@ if 'QONDOR_BATCHMODE' in os.environ:
 from .schedd import get_best_schedd, get_schedd_ads
 from .preprocess import Preprocessor
 from .submit import SHFile, Submitter
+
+
+if os.environ.get('HOSTNAME', '').endswith('fnal.gov'):
+    # Fix to be able to import htcondor python bindings
+    import sys
+    sys.path.extend([
+        '/usr/lib64/python2.6/site-packages',
+        '/usr/lib64/python2.7/site-packages'
+        ])
