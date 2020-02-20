91d80b3bfe137fe25f8b8b7dec9781d2039a59ac
Thomas Klijnsma <klijnsma@fnal.gov>
2020-02-20 17:18:21 -0600

    updated setup.py for new version

--- setup.py
+++ setup.py
@@ -2,17 +2,16 @@ from setuptools import setup
 
 setup(
     name          = 'qondor',
-    version       = '0.1',
+    version       = '0.2',
     license       = 'BSD 3-Clause License',
     description   = 'Description text',
     url           = 'https://github.com/tklijnsma/qondor.git',
-    download_url  = 'https://github.com/tklijnsma/qondor/archive/v0_1.tar.gz',
+    download_url  = 'https://github.com/tklijnsma/qondor/archive/v0_2.tar.gz',
     author        = 'Thomas Klijnsma',
     author_email  = 'tklijnsm@gmail.com',
     packages      = ['qondor'],
     zip_safe      = False,
     scripts       = [
-        # 'bin/qondor-run'
         'bin/qondor-submit',
         'bin/qondor-sleepuntil'
         ],
