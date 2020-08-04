29ac4bff62055e55140a4b531116ab70634a665b
Thomas Klijnsma <klijnsma@fnal.gov>
2020-08-04 11:02:57 -0500

    standard htcondor installations for lpc do not work for python 3

--- qondor/__init__.py
+++ qondor/__init__.py
@@ -31,10 +31,11 @@ if BATCHMODE and osp.isfile(Preprocessor.LS_CACHE_FILE):
 if os.environ.get('HOSTNAME', '').endswith('fnal.gov'):
     # Fix to be able to import htcondor python bindings
     import sys
-    sys.path.extend([
-        '/usr/lib64/python2.6/site-packages',
-        '/usr/lib64/python2.7/site-packages'
-        ])
+    if sys.version_info.major < 3:
+        sys.path.extend([
+            '/usr/lib64/python2.6/site-packages',
+            '/usr/lib64/python2.7/site-packages'
+            ])
     schedd.GLOBAL_SCHEDDMAN_CLS = schedd.ScheddManagerFermiHTC
     seutils.set_default_mgm('root://cmseos.fnal.gov')
 
